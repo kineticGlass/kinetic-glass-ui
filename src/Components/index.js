730e0450329f32f16729184889d91b27c5aa10c7
abdulman7823 <abdulman4401@gmail.com>
2023-01-11 04:24:35 +0500

    export-all

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,6 @@ export * from './forms';
 export * from './typography';
 export * from './layouts';
 export * from './media-icons';
+export * from './cards';
+export * from './data-display';
+export * from './navigation';
