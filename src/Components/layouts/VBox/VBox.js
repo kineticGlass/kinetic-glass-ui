61d6f17485801d8296154942dd4d647c90310b44
abdulman7823 <abdulman4401@gmail.com>
2023-01-11 03:32:06 +0500

    layout-component-initialization

---
+++ src/Components/layouts/VBox/VBox.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function VBox() {
+  return (
+    <div>VBox</div>
+  )
+}
---
+++ src/Components/layouts/index.js
@@ -0,0 +1,7 @@
+export * from './Border';
+export * from './Box';
+export * from './Grid';
+export * from './HBox';
+export * from './Split';
+export * from './VBox';
+export * from './Wrap'
